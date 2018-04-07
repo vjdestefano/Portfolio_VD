be1a793bdecb302cf74d91f80d1792422a6d5eb1
Vinny DeStefano <vjdestefano500@gmail.com>
2018-04-07 13:28:28 -0400

    made more functionality

--- portfolioWork.js
+++ portfolioWork.js
@@ -1,9 +1,26 @@
 
-
-
 $(document).ready(function() {
   var profPic = $('<img>').attr("src","./images/profile-pic.jpg");
   profPic.attr("width",($('#testWidth').innerWidth() * 1));
-  profPic.attr("height",($('#testWidth').innerWidth() * 1));
+  profPic.attr("height",($('#testWidth').innerWidth() * .75));
   $("#testWidth").append(profPic)
 });
+
+$("#liriInput").on("keyup", function(event){
+event.preventDefault();
+if(event.key === "Enter"){
+  var liriInput = $("#liriInput").val();
+  console.log(liriInput);
+  $("#liriInput").val('');
+}
+});
+
+
+$("#askLiri").on("click", function(event){
+  event.preventDefault();
+  var liriInput2 = $("#liriInput").val();
+  console.log(liriInput2);
+  $("#liriInput").val('');
+  $("#testAnim").slideDown(750);
+
+})
